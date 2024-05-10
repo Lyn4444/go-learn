bf0dfa46885a3a4e0c75d51a9f9c3d576d5e0a29
lyn4444 <3077002706@qq.com>
2024-05-10 16:14:23 +0800

    init

--- src/learn/commonValue.go
+++ src/learn/commonValue.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"fmt"
+	"math"
+)
+
+const s string = "constant"
+
+func main() {
+	fmt.Println(s)
+
+	// 数值型常量没有确定的类型，直到被给定某个类型，比如显式类型转化
+	const n = 50000
+	const d = 3e20 / n
+
+	fmt.Println(d)
+
+	fmt.Println(int64(d))
+
+	fmt.Println(math.Sin(n))
+
+}
